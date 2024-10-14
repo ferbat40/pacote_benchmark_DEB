2eefd581d473e1bdd6c5dea19da09df135663096
Fernando Silva <setec.solda@gmail.com>
2024-10-13 22:23:09 -0300

    alt5

--- DTLZ1.py
+++ DTLZ1.py
@@ -1,7 +1,13 @@
+import numpy as np
+
 class DTLZ1:
         
 
     def exibir(self):
         K = self.N-self.M+1
         Nvar = K+self.M-1
+        S = self.P=1500
+        Point = [*np.random.random((S,Nvar+1))]
         print(f"valor de k = {K} , valor de Nvar = {Nvar}")
+        for i in Point:
+            print(i)
