3bfa122ed67848dfde1d44f445558c0b4b8b7c17
Fernando Silva <setec.solda@gmail.com>
2024-10-13 22:16:47 -0300

    alt5

--- DTLZ1.py
+++ DTLZ1.py
@@ -4,4 +4,4 @@ class DTLZ1:
     def exibir(self):
         K = self.N-self.M+1
         Nvar = K+self.M-1
-        print(f"valor de k {K} , valor de Nvar {Nvar}")
+        print(f"valor de k = {K} , valor de Nvar = {Nvar}")
