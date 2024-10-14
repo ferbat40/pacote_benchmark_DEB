1b8023a55fdb31a819ae868ec623750985a9a5f1
Fernando Silva <setec.solda@gmail.com>
2024-10-13 22:29:02 -0300

    alt5

--- DTLZ1.py
+++ DTLZ1.py
@@ -9,5 +9,8 @@ class DTLZ1:
         S = self.P=1500
         Point = [*np.random.random((S,Nvar+1))]
         print(f"valor de k = {K} , valor de Nvar = {Nvar}")
+        for i_g in Point:
+            i_g[self.M:]=0.5
+
         for i in Point:
-            print(i)
+            print(i) 
