991c051b7cbc2f59e3447802fd4642cb99ff1ca3
Fernando Silva <setec.solda@gmail.com>
2024-10-16 10:09:58 -0300

    sd1

--- create_benchmark.py
+++ create_benchmark.py
@@ -15,25 +15,7 @@ class CreateBenchmark(InitBenchmark):
                 2:  self.call_DTLZ2,
                 }
             
-    def show_fo_in(self):
-        print(f'len = {len(self.get_fo_in())}')
-        for i in self.get_fo_in():
-            print(i)
-
-    def show_fo_out(self):
-        print(f'len = {len(self.get_fo_out())}')
-        for i in self.get_fo_out():
-            print(i)
-
-    def show_fo_out_g(self):
-        print(f'len = {len(self.get_fo_out_g())}')
-        for i in self.get_fo_out_g():
-            print(i)
-
-    def show_var(self):
-        print(f'Nvar = {self.get_Nvar()} , K = {self.get_K()} , M = {self.get_M()} , N = {self.get_N()}')
         
-     
     def call_DTLZ1(self):
         self.set_DTLZ(DTLZ1(self))
 
@@ -46,7 +28,25 @@ class CreateBenchmark(InitBenchmark):
         if self.benchmark in self.PARAM:
            self.PARAM[self.benchmark]()
 
-  
+
+
+    def plot_graphic_configure(self,vet_0=[],vet_1=[],vet_3=[]):
+        fig = plt.figure()
+        fig = plt.figure(figsize=(10, 15))
+        ax = fig.add_subplot(111, projection='3d')
+        ff = np.array([fp[0:] if len(fp) > 0 else [0,0,0] for fp in vet_0])
+        pp = np.array([fp[0:] if len(fp) > 0 else [0,0,0] for fp in vet_1])
+        cp = np.array([cv[0:] if len(cv) > 0 else [0,0,0] for cv in vet_3])
+        print(pp.shape,cp.shape,ff.shape)
+        if (len(pp) >0):
+            ax.scatter(pp[:,0],pp[:,1],pp[:,2],color='red')
+        if (len(cp) >0):
+            ax.scatter(cp[:,0],cp[:,1],cp[:,2],color='gray')
+        ax.scatter(ff[:,0],ff[:,1],ff[:,2])
+        ax.view_init(elev=360, azim=25)
+        plt.show()
+
+        
     
     def plot_graphic_in_G(self):
         fig = plt.figure()
@@ -54,7 +54,7 @@ class CreateBenchmark(InitBenchmark):
         ax = fig.add_subplot(111, projection='3d')
         pp = np.array([fp[0:] if len(fp) > 0 else [0,0,0] for fp in self.get_fo_in() ])
         cp = np.array([cv[0:] if len(cv) > 0 else [0,0,0] for cv in self.get_fo_out() ])
-        print(pp.shape,cp.shape,self.get_M())
+        print(pp.shape,cp.shape)
         if (len(cp)>0):
             ax.scatter(cp[:,0],cp[:,1],cp[:,2],color='gray')
         if (len(pp)>0):
@@ -70,7 +70,7 @@ class CreateBenchmark(InitBenchmark):
         ff = np.array([fp[0:] if len(fp) > 0 else [0,0,0] for fp in self.get_fo_out_g()])
         pp = np.array([fp[0:] if len(fp) > 0 else [0,0,0] for fp in self.get_fo_in() ])
         cp = np.array([cv[0:] if len(cv) > 0 else [0,0,0] for cv in self.get_fo_out()  ])
-        print(pp.shape,cp.shape,ff.shape,self.get_M())
+        print(pp.shape,cp.shape,ff.shape)
         if (len(pp) >0):
             ax.scatter(pp[:,0],pp[:,1],pp[:,2],color='red')
         if (len(cp) >0):
@@ -78,7 +78,8 @@ class CreateBenchmark(InitBenchmark):
         ax.scatter(ff[:,0],ff[:,1],ff[:,2])
         ax.view_init(elev=360, azim=25)
         plt.show()
-  
+
+
   
 
 #benchmark = CreateBenchmark(1,1500,45,3)
@@ -86,11 +87,10 @@ class CreateBenchmark(InitBenchmark):
 
 
 #benchmark.get_DTLZ().build_objective_space_in_G()
-#benchmark.plot_graphic_in_G()
-#benchmark.show_fo_in()
-#benchmark.show_fo_out()
-#benchmark.show_fo_out_g()
-#benchmark.show_var()
+#dados=benchmark.get_fo_out()
+#benchmark.plot_graphic_configure(dados)
+#print(benchmark.get_fo_out())
+
 
 
 
