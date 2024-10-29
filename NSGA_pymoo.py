e02b8a45fd4938ace37d018e2a40e3e215db3fb6
Fernando Silva <setec.solda@gmail.com>
2024-10-28 22:45:31 -0300

    sdf

--- NSGA_pymoo.py
+++ NSGA_pymoo.py
@@ -9,15 +9,14 @@ from pymoo.util.ref_dirs import get_reference_directions
                            
 
 class NSGAPymoo(Problem):
-    def __init__(self,init_benchmark,population=300, generations=100):
+    def __init__(self,init_benchmark,population=100, generations=300):
         
         self.init_benchmark=init_benchmark
         xl = np.full(self.init_benchmark.get_Nvar(),0)
         xu = np.full(self.init_benchmark.get_Nvar(),1)
         self.DTLZ1=DTLZ1(self.init_benchmark)    
+        self.generations=generations
         super(). __init__(n_var=self.init_benchmark.get_Nvar(), n_obj=self.init_benchmark.get_M(), n_ieq_constr=1, xl=xl, xu=xu)
-
-
 
     
     def calc_f(self,x,Gxm,prod_xm1=[],prod_xm2=[]):
@@ -31,7 +30,6 @@ class NSGAPymoo(Problem):
         prod_xm2=np.array([np.prod(xm2_p[linha,0:xm2_p.shape[1]])  for index,linha in enumerate(range(xm2_p.shape[0]))])
         prod_xm2=prod_xm2.reshape(xm2_p.shape[0],1)
 
-
         
         x1=np.array(x[:,0])
         x1=x1.reshape(x.shape[0],1)
@@ -42,49 +40,22 @@ class NSGAPymoo(Problem):
         f1=1/2*prod_xm1*(1+Gxm)
         f2=1/2*prod_xm2*(1-xm1)*(1+Gxm)
         f3=1/2*(1-x1)*(1+Gxm)
-        return f1,f2,f3
-       
+        return f1,f2,f3     
         
     
     def calc_g(self,x,G=[]):
          Gxm=np.array(x[:,self.init_benchmark.get_M()-1:])
-         #for index,linha in enumerate(range(Gxm.shape[0])):
-            # G.append(100*((self.init_benchmark.get_K())+np.sum([((XeXm-0.5)**2)-(np.cos(20*np.pi*(XeXm-0.5))) for XeXm in Gxm[linha,0:len(Gxm)]])))
-         #G=np.array(G)  
-         #G=G.reshape((Gxm.shape[0],1))
-         #print("shap",Gxm.shape)
-
-
-         #G = np.array(100*((self.init_benchmark.get_K())+np.sum((XeXm-0.5)**2)-(np.cos(20*np.pi*(XeXm-0.5))) for XeXm in Gxm[row,0:Gxm.shape[1]]) for index,row in enumerate(range(Gxm.shape[0]))))
-         #G=G.reshape((Gxm.shape[0],1))
          G = np.array([ 100* ((self.init_benchmark.get_K())+np.sum(((XeXm-0.5)**2)-(np.cos(20*np.pi*(XeXm-0.5))) for XeXm in Gxm[row,0:Gxm.shape[1]]))  for index,row in enumerate(range(Gxm.shape[0]))  ])
-         #G=G.reshape((Gxm.shape[0],1))
-        
-
-
-         
-
-
-
-
-
          return G.reshape((Gxm.shape[0],1))
     
-    def constraits(self,f):
+    def constraits(self,f,f_c=[]):
         f_constraits=np.array(f)
-        f_c=[]
-        for index,linha in enumerate(range(f_constraits.shape[0])):
-            f_c.append(np.sum([ f_c  for  f_c in f_constraits[linha,0:f_constraits.shape[1]]])-0.6)
-        
-        f_c=np.array(f_c)
-        f_c=f_c.reshape(f_constraits.shape[0],1)
-        return f_c
+        f_c = np.array([np.sum([ f_c  for  f_c in f_constraits[linha,0:f_constraits.shape[1]]])-0.6 for index,linha in enumerate(range(f_constraits.shape[0]))  ])
+        return f_c.reshape(f_constraits.shape[0],1)
     
 
 
-    def _evaluate(self, x, out, *args, **kwargs):
-
-        
+    def _evaluate(self, x, out, *args, **kwargs):   
         Gxm=self.calc_g(x)
         F=self.calc_f(x,Gxm)
         F_join = np.column_stack([F[0],F[1],F[2]])
@@ -94,7 +65,7 @@ class NSGAPymoo(Problem):
         
 
     def exec(self):
-        ref_dirs = get_reference_directions("das-dennis", 3, n_partitions=30)
+        ref_dirs = get_reference_directions("das-dennis", self.init_benchmark.get_M(), n_partitions=30)
         popsize = ref_dirs.shape[0] + ref_dirs.shape[0] % 4
         nsga3 = NSGA3(ref_dirs, pop_size=popsize)
             
@@ -103,15 +74,13 @@ class NSGAPymoo(Problem):
         res_NSGA = minimize(
             NSGAPymoo(self.init_benchmark),
             nsga3,
-            ('n_gen', 300),
+            ('n_gen', self.generations),
             seed=SEED,
             save_history=True,
             verbose=False
             )      
         
-        fo=np.column_stack([res_NSGA.F])
-        
-        return fo
+        return np.column_stack([res_NSGA.F])
 
 
         
--- create_benchmark.py
+++ create_benchmark.py
@@ -97,7 +97,7 @@ class CreateBenchmark(InitBenchmark):
           
      
 
-bk = CreateBenchmark(1, 1000,6,3)
+#bk = CreateBenchmark(1, 1000,6,3)
 #bk.call_benchmark()
 #var1=bk.get_DTLZ().build_in_G()
 #var2=bk.get_DTLZ().build_out_G()
@@ -119,10 +119,10 @@ bk = CreateBenchmark(1, 1000,6,3)
 #k.call_plot_PF_M(pt1,pt2,pt3)
 #plot.plot_FP_M("Objetivo_1","Objetivo_2","Objetivo_3")
 
-NSGAPy = NSGAPymoo(bk)
-pt_nsga= NSGAPy.exec()
-pt_nsga=np.array(pt_nsga)
-print(pt_nsga)
+#NSGAPy = NSGAPymoo(bk)
+#pt_nsga= NSGAPy.exec()
+#pt_nsga=np.array(pt_nsga)
+#print(pt_nsga)
 
 
 #print(pt_nsga)
