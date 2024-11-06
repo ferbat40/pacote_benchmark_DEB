dce4b44a7a075b4dbddc697d06a217e1b7c70af6
Fernando <setec.solda@gmail.com>
2024-11-06 17:44:21 -0300

    sdf

--- SPEA_pymoo.py
+++ SPEA_pymoo.py
@@ -4,15 +4,18 @@ from pymoo.algorithms.moo.spea2 import SPEA2
 from pymoo.optimize import minimize
 import numpy as np
 from pymoo.util.ref_dirs import get_reference_directions
+from pymoo.operators.crossover.sbx import SBX
+from pymoo.operators.mutation.pm import PolynomialMutation
 
                            
 
 class SPEAPymoo(Problem):
-    def __init__(self,benchmark,partitions=15, generations=300,seed=15):
+    def __init__(self,benchmark,partitions=15, generations=300,seed=15,pop_size=100):
         self.benchmark=benchmark
         self.partitions=partitions
         self.generations=generations
         self.seed=seed
+        self.pop_size=pop_size
         xl = np.full(self.benchmark.get_Nvar(),0)
         xu = np.full(self.benchmark.get_Nvar(),1)
         self.DTLZ=self.benchmark.get_DTLZ()
@@ -23,17 +26,25 @@ class SPEAPymoo(Problem):
         Gxm=self.DTLZ.calc_g(x)
         F=self.DTLZ.calc_f(x,Gxm)
         out["F"]=F
-        f_c=self.DTLZ.constraits(F,0.60)
+        f_c=self.DTLZ.constraits(F,0.55)
         out["G"]=f_c
         
 
     def exec(self):
-        #ref_dirs = get_reference_directions("das-dennis", self.benchmark.get_M(), n_partitions=self.partitions)
-        #popsize = ref_dirs.shape[0] + ref_dirs.shape[0] % 4
-        spea2 = SPEA2(pop_size=300)
+
+        mutation = PolynomialMutation(prob=0.1, eta=15)
+        crossover = SBX(prob=0.9, eta=15)
+        spea2 = SPEA2(pop_size=300,
             
-        res_NSGA = minimize(
-            SPEAPymoo(self.benchmark),
+            crossover=crossover,
+            mutation=mutation
+                      
+                      
+                      
+                      )
+            
+        res_SPEA = minimize(
+            SPEAPymoo(self.benchmark, pop_size=self.pop_size),
             spea2,
             ('n_gen', self.generations),
             seed=self.seed,
@@ -41,4 +52,4 @@ class SPEAPymoo(Problem):
             verbose=False
             )      
         
-        return np.column_stack([res_NSGA.F])
+        return np.column_stack([res_SPEA.F])
