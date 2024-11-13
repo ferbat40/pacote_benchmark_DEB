d44382a95b4575deaba0ffc08d03b901b7b56b1c
Fernando <setec.solda@gmail.com>
2024-11-13 17:47:33 -0300

    dfs

--- DTLZ2.py
+++ DTLZ2.py
@@ -1,5 +1,37 @@
+import numpy as np
+
 class DTLZ2:
 
 
     def __init__(self,new_benchmark_obj):
-        print("aqui")
+        self.new_benchmark_obj=None
+        self.new_benchmark_obj=new_benchmark_obj
+
+
+    def calc_f(self,x,Gxm,prod_xm1=[],prod_xm2=[]):
+        F_index=[]
+        for v in range(0,self.new_benchmark_obj.get_M()):
+            F_index.append(v)   
+        xm2_p=np.array(x[:,:self.new_benchmark_obj.get_M()-2])
+        prod_xm2=np.array([np.prod(np.cos(xm2_p[linha,0:xm2_p.shape[1]])*np.pi/2)  for index,linha in enumerate(range(xm2_p.shape[0]))])
+        
+        print("x",x)
+        print("xm2_p",xm2_p)
+        print("prod_xm2",prod_xm2)
+
+
+    def calc_g(self,x=[],G=[]):
+         Gxm=np.array(x[:,self.new_benchmark_obj.get_M()-1:])
+         G = np.array([np.sum((XeXm-0.5)**2 for XeXm in Gxm[row,0:Gxm.shape[1]]) for index, row in enumerate(range(Gxm.shape[0]))])
+         return G.reshape((Gxm.shape[0],1))
+
+              
+         
+
+
+    def minimize_DTLZ(self):
+         x=np.array(self.new_benchmark_obj.get_Point_in_G())
+         g=self.calc_g(x)
+         f=self.calc_f(x,g)
+         
+         
--- teste.py
+++ teste.py
@@ -7,63 +7,16 @@ from MOEAD_pymoo import MOEADpymoo
 
 
 
-bk = CreateBenchmark(1, 200,6,3)
+bk = CreateBenchmark(2, 3,15,5)
 bk.call_benchmark()
 
 points_in=bk.get_DTLZ().minimize_DTLZ()
-points_out=bk.get_DTLZ().maximize_DTLZ()
-
-
-
-
-
-NSGAP = NSGAPymoo(bk)
-pt_nsga= NSGAP.exec()
-
-
-
-#metric = Metrics()
-#metric.add_t(NSGAP)
-#metric.add_t(pt_nsga)
-#metric.add_t(points_in)
+#points_out=bk.get_DTLZ().maximize_DTLZ()
 
 
 
 
 
 
-#SPEA = SPEAPymoo(bk)
-#pt_spea= SPEA.exec()
-#metric.add_t(SPEA)
-#metric.add_t(pt_spea)
 
-#bk.show_points(points_in)
-#bk.show_points(pt_nsga)
-#bk.show_points(pt_spea)
-
-#RVEA_= RVEAymoo(bk)
-#pt_rvea = RVEA_.exec()
-#print(pt_rvea)
-#metric.add_t(RVEA_)
-#metric.add_t(pt_rvea)
-
-#MOEAD_= MOEADpymoo(bk)
-#pt_moead = MOEAD_.exec()
-#print(pt_moead)
-#metric.add_t(MOEAD_)
-#metric.add_t(pt_moead)
-
-#bk.call_plot_PF_M(points_in,pt_nsga)
-ff=bk.show_points(pt_nsga)
-
-#bk.call_plot_PF_M(pt_rvea,pt_moead,pt_nsga)
-
-
-
-
-#pd_metric=metric.get_metric()
-#print("pd_metric")
-#print(pd_metric)
-
-
-#print(bk.get_Nvar(), bk.get_M(), bk.get_K(), bk.get_constraits_default(), bk.get_constraits_NSGA_3(), bk.get_constraits_SPEA_2())
+print(bk.get_Nvar(), bk.get_M(), bk.get_K(), bk.get_constraits_default(), bk.get_constraits_NSGA_3(), bk.get_constraits_SPEA_2())
