d4e4050de948e883330e37605510ef9115ab77d1
Fernando <setec.solda@gmail.com>
2024-11-20 22:09:10 -0300

    adsf

--- DTLZ7.py
+++ DTLZ7.py
@@ -10,23 +10,16 @@ class DTLZ7:
 
 
     def calc_h(self,x,f,g):
-        gaux = np.tile(g, (1, self.new_benchmark_obj.get_M()-1))
-        #print("gaux",gaux)
-        #print("g",g)
-        #print("f",f)
-        h=np.hstack([f[:,col:col+1]/(1+gaux)*(1+np.sin(3*np.pi*f[:,col:col+1])) for col in range(0,f.shape[1])])
-        #h=np.hstack([f[:,col:col+1]/(1+g)*(1+np.sin(3*np.pi*f[:,col:col+1])) for col in range(0,f.shape[1])])
-        #print("h normal",h)
-        #print("h_aux",h_aux)
+        h=np.hstack([f[:,col:col+1]/(1+g)*(1+np.sin(3*np.pi*f[:,col:col+1])) for col in range(0,f.shape[1])])
         h_sum= np.array(np.sum(h,axis=1)).reshape(h.shape[0],1)
-        h_m=self.new_benchmark_obj.get_M()-h_sum
-        return h_m
+        #h_m=self.new_benchmark_obj.get_M()-h_sum
+        return h_sum
 
 
 
     def calc_Fm(self,f,x,Gxm):
         h=self.calc_h(x,f,Gxm)
-        Fm=(1+Gxm)*h  
+        Fm=(1+Gxm)*self.new_benchmark_obj.get_M()-h
         return Fm
 
 
@@ -42,7 +35,7 @@ class DTLZ7:
     def calc_g(self,x=[],G=[]):
         Gxm=np.array(x[:,self.new_benchmark_obj.get_M()-1:])
         g_sum = np.array([ (np.sum(Gxm[row, :]))  for row in range(Gxm.shape[0])]).reshape(Gxm.shape[0],1)
-        g=1+9/self.new_benchmark_obj.get_K()*g_sum
+        g=1+(9*g_sum)/self.new_benchmark_obj.get_K()
         return g
      
     def minimize_DTLZ(self):
