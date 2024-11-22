b28df9734083de76c44cd4c2f05bbbb9090e8bb0
Fernando <setec.solda@gmail.com>
2024-11-22 13:43:10 -0300

    dsf

--- DTLZ7.py
+++ DTLZ7.py
@@ -10,13 +10,8 @@ class DTLZ7:
 
 
     def calc_h(self,x,f,g):
-        f=np.array(x[:,self.new_benchmark_obj.get_M()-1:])
         h=np.hstack([f[:,col:col+1]/(1+g)*(1+np.sin(3*np.pi*f[:,col:col+1])) for col in range(0,f.shape[1])])
         h_sum= np.array(np.sum(h,axis=1)).reshape(h.shape[0],1)
-        #print(g)
-        #print(h)
-        #print(x)
-        #h_m=self.new_benchmark_obj.get_M()-h_sum
         return h_sum
 
 
