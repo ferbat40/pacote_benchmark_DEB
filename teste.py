c8f68630fdcda81f7ed3cec7902e6eb5361ec05d
Fernando <setec.solda@gmail.com>
2024-12-01 16:39:32 -0300

    sdf

--- teste.py
+++ teste.py
@@ -7,7 +7,7 @@ from MOEAD_pymoo import MOEADpymoo
 
 
 
-bk = CreateBenchmark(8,1500,3,3)
+bk = CreateBenchmark(8,1500,10,3)
 
 
 
@@ -17,9 +17,9 @@ points_in=bk.get_DTLZ().minimize_DTLZ()
 #points_out=bk.get_DTLZ().maximize_DTLZ()
 bk.show_points(points_in)
 #bk.show_points(points_out)
-NSGA = NSGAPymoo(bk,40,500)
-pt_NSGA =NSGA.exec()
-bk.show_points(pt_NSGA )
+#NSGA = NSGAPymoo(bk,40,500)
+#pt_NSGA =NSGA.exec()
+#bk.show_points(pt_NSGA )
 
 
 #SPEA_2 = SPEAPymoo(bk,30,500)
