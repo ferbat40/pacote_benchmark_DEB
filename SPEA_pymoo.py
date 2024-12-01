9d15c4c50c27a369390ce511e5ebf002ec434c77
Fernando <setec.solda@gmail.com>
2024-12-01 16:49:08 -0300

    sfd

--- SPEA_pymoo.py
+++ SPEA_pymoo.py
@@ -35,7 +35,7 @@ class SPEAPymoo(Problem):
             gjx_const=self.DTLZ.const_gjx(fjx,self.benchmark.get_M())
             gmx_const=self.DTLZ.const_gmx(fjx,fix,self.benchmark.get_M())
             constraits_g=np.column_stack([gjx_const,gmx_const])
-            constraits_g_v = constraits_g-1
+            constraits_g_v = constraits_g
             out["G"]=-constraits_g_v
         
 
