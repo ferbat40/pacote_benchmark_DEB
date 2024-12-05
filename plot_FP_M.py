e3b6a4356dddd4bc86b1c5fa63a8f821b3617fc0
Fernando <setec.solda@gmail.com>
2024-12-05 14:33:11 -0300

    dfs

--- plot_FP_M.py
+++ plot_FP_M.py
@@ -46,7 +46,7 @@ class PlotFP_M():
          
          colors  = ['red','gray','blue']
          vectors = self.labels         
-         fig = plt.figure(figsize=(9, 30))
+         fig = plt.figure(figsize=(9, 9))
          ax = fig.add_subplot(111, projection='3d')
 
 
